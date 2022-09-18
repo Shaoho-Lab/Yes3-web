539846f8aef2220b461950683341628a5ab35a7f
Will Jiang <willjiang2018@gmail.com>
2022-09-17 19:53:22 -0700

    incorporate tree graph

---
+++ packages/lyon-frontend/src/components/template-tree/index.tsx
@@ -0,0 +1,57 @@
+import classNames from 'classnames'
+import { useState } from 'react'
+import styles from './index.module.scss'
+import { Graph } from 'react-d3-graph'
+
+export interface TemplateTreeProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const data = {
+  nodes: [{ id: 'Vitalik' }, { id: 'Jason' }, { id: 'zh3036' }, { id: 'Will' }, { id: 'Aego' }, { id: 'Yan' }],
+  links: [
+    { source: 'Vitalik', target: 'Jason' },
+    { source: 'Jason', target: 'zh3036' },
+    { source: 'Jason', target: 'Will' },
+    { source: 'Aego', target: 'zh3036' },
+    { source: 'Aego', target: 'Will' },
+    { source: 'Yan', target: 'Jason' },
+    { source: 'Will', target: 'zh3036' },
+  ],
+}
+
+const myConfig = {
+  nodeHighlightBehavior: true,
+  node: {
+    color: 'lightgreen',
+    size: 120,
+    highlightStrokeColor: 'blue',
+  },
+  link: {
+    highlightColor: 'lightblue',
+  },
+}
+
+const onClickNode = function (nodeId: string) {
+  window.alert(`Clicked node ${nodeId}`)
+}
+
+const onClickLink = function (source: string, target: string) {
+  window.alert(`Clicked link between ${source} and ${target}`)
+}
+
+const TemplateTree = ({ className, ...props }: TemplateTreeProps) => {
+  return (
+    <div className={classNames(styles.templateTree, className)} {...props}>
+      <Graph
+        id="graph-id" // id is mandatory
+        data={data}
+        config={myConfig}
+        onClickNode={onClickNode}
+        onClickLink={onClickLink}
+      />
+      ;
+    </div>
+  )
+}
+
+export default TemplateTree
--- packages/lyon-frontend/src/pages/template-view/index.tsx
+++ packages/lyon-frontend/src/pages/template-view/index.tsx
@@ -1,6 +1,7 @@
 import Checkbox from 'components/checkbox'
 import CommonLayout from 'components/common-layout'
 import styles from './index.module.scss'
+import TemplateTree from 'components/template-tree'
 
 const TemplateViewPage = () => {
   return (
@@ -8,13 +9,23 @@ const TemplateViewPage = () => {
       <div className={styles.heading}>
         Am I a good teammate in ETH Global Hackathon?
       </div>
-      <div className={styles.options}>
-        <Checkbox label="Set as a private question" />
-        <Checkbox label="Generate a password" />
-      </div>
-      <div className={styles.buttons}>
-        <div className={styles.cancel} onClick={() => window.history.back()}>
-          Back
+      <div className={styles.content}>
+        <h1>Am I a good teammate in ETH Global Hackathon?</h1>
+        <div className={styles.options}>
+          <Checkbox label="Set as a private question" />
+          <Checkbox label="Generate a password" />
+        </div>
+        <div className={styles.buttons}>
+          <div className={styles.cancel} onClick={() => window.history.back()}>
+            Back
+          </div>
+          <div className={styles.confirm}>Ask the question</div>
+        </div>
+        <div className={styles.charts}>
+          <div className={styles.stats}></div>
+          <div className={styles.graph}>
+            <TemplateTree />
+          </div>
         </div>
         <div className={styles.confirm}>Ask the question</div>
       </div>
