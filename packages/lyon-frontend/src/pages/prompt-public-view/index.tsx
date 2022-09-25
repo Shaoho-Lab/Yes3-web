1db278fc241dab5512ea59d7005ca28ee18ebeed
Feng Yan <fy48@duke.edu>
2022-09-24 23:26:41 -0400

    added metadata

--- packages/lyon-frontend/src/pages/prompt-public-view/index.tsx
+++ packages/lyon-frontend/src/pages/prompt-public-view/index.tsx
@@ -120,7 +120,7 @@ const PromptPublicViewPage = () => {
             const name = getName(userAddressNameMapping, key)
             const value = chosenReplies[key]
 
-            chosenRepliesTemp.push(name + ": " + value.replierDetail)
+            chosenRepliesTemp.push(name + ': ' + value.replierDetail)
           }
 
           setChosenRepliesList(chosenRepliesTemp)
